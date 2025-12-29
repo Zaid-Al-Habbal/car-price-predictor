5a306f0a7a003852a2dded70a83cc999e4f1722f
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-12-29 16:00:25 +0300

    begin with backend and setup schema for car features

---
+++ back-end/app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from typing import Literal
+
+
+class CarFeatures(BaseModel):
+    name: str
+    year: int
+    km_driven: int
+    fuel: Literal['Diesel', 'Petrol', 'CNG', 'LPG']
+    seller_type: Literal['Individual', 'Dealer', 'Trustmark Dealer']
+    transmission: Literal['Manual', 'Automatic']
+    owner: Literal['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner', 'Test Drive Car']
+    mileage: str
+    engine: str
+    max_power: str
+    seats: float
+
